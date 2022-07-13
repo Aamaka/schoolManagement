c5331107db37642dd78a5cc28ca74cd0d5c4afe4
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-13 19:18:05 +0100

    school

--- src/main/java/africa/semicolon/data/model/Staffs.java
+++ src/main/java/africa/semicolon/data/model/Staffs.java
@@ -1,5 +1,11 @@
 package africa.semicolon.data.model;
 
 public class Staffs {
-
+    private String name;
+    private String phoneNumber;
+    private String address;
+    private String email;
+    private String gender;
+    private int age;
+    private StaffType type;
 }
