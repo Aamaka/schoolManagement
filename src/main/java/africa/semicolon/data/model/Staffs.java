326014f463ab3ac2401a74f75e55e141c677e43f
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-13 19:41:51 +0100

    school

--- src/main/java/africa/semicolon/data/model/Staffs.java
+++ src/main/java/africa/semicolon/data/model/Staffs.java
@@ -1,8 +1,10 @@
 package africa.semicolon.data.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
@@ -15,5 +17,6 @@ public class Staffs {
     private String gender;
     private int age;
     private StaffType type;
+    @Id
     private String id;
 }
