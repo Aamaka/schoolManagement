5d54f032a8459182a26d86d618796829793b400d
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-14 14:46:01 +0100

    school

--- src/main/java/africa/semicolon/data/model/Student.java
+++ src/main/java/africa/semicolon/data/model/Student.java
@@ -31,4 +31,7 @@ public class Student {
     @NonNull
     private StudentType student;
 
+    @NonNull
+    private int age;
+
 }
--- src/main/java/africa/semicolon/dtos/Requests/RegisterRequest.java
+++ src/main/java/africa/semicolon/dtos/Requests/RegisterRequest.java
@@ -1,5 +1,19 @@
 package africa.semicolon.dtos.Requests;
 
+import africa.semicolon.data.model.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
+    private String name;
+    private String stateOfOrigin;
+    private String dob;
+    private String grade;
+    private int age;
+    private Gender gender;
 }
