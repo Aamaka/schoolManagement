87acc23e40f54af9446bef44daf47fbc18973db9
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-15 10:20:27 +0100

    school

--- src/main/java/africa/semicolon/dtos/Responses/LoginResponse.java
+++ src/main/java/africa/semicolon/dtos/Responses/LoginResponse.java
@@ -1,5 +1,13 @@
 package africa.semicolon.dtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
 
+    private String message;
 }
